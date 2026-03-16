4ab7ea76c0744a05e0793134ec1e785bd322eb4b
Moustapha <moustaf478@gmail.com>
2026-03-16 03:45:30 +0000

    Finalisation API avec MongoDB JWT

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109841df818c22ab3ea1a5af30c82cfb1772ced9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
